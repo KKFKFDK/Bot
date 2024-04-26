734bc8023da72095bba9fb359db98283fd75380d
George Tuskaev <viven260@gmail.com>
2024-04-26 16:13:48 +0300

    fix: webdriver fix

--- bot.py
+++ bot.py
@@ -1,28 +1,34 @@
-import discord
-from discord.ext import commands
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+
 intents = discord.Intents.default()
+
 intents.typing = False
 intents.members = True
+
 intents.presences = False
 
-bot = commands.Bot(command_prefix='/', intents=intents)
+intents.members = True
+
+bot = commands.Bot(command_prefix='/', intents=discord.Intents.all())
 
 
 @bot.command()
 async def forum(ctx):
+    #конфиг
     options = Options()
+    options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
 
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-    try:
-        driver.get("https://python.org")
-        page_title = driver.title
-        await ctx.send(f"Заголовок страницы: {page_title}")
-    finally:
-        driver.quit()
+    driver.get("https://python.org")
+    page_title = driver.title
+
+    await ctx.send(f"Заголовок страницы: {page_title}")
+
+    driver.close()
